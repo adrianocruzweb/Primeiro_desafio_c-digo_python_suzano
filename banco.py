fccf0d8eea01bb50f3022d544e44565b62c16e96
adrianocruzweb <adrianocruzweb@hotmail.com>
2025-09-09 21:06:13 -0400

    fist while operations

--- banco.py
+++ banco.py
@@ -13,3 +13,19 @@ extrato = ""
 numero_saques = 0
 LIMITE_SAQUES = 3
 
+while True:
+
+    opcao = input(menu)
+
+    if opcao == "d":
+        valor = float(input("Informe o valor do saque: "))
+        print(f"Deposito {valor}.")
+    elif opcao == "s":
+        valor = float(input("Informe o valor do saque: "))
+        print(f"Saque {valor}.")
+    elif opcao == "e":
+        print("Extrato.")
+    elif opcao == "q":
+        break
+    else:
+        print("Erro.")
